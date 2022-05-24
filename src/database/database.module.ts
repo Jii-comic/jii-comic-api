f26087e8e38897e93c10c9b855c27c93f182846d
altourius <nhattung1709@gmail.com>
2022-05-24 10:03:12 +0700

    reset db config

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,25 +9,14 @@ import { UsersModule } from "src/users/users.module";
             imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => {
-                let dbInfo;
-                console.log(configService.get("DATABASE_URL"));
-                if (configService.get("DATABASE_URL")) {
-                    dbInfo = {
-                        url: configService.get("DATABASE_URL"),
-                    };
-                } else {
-                    dbInfo = {
-                        host: configService.get("DATABASE_HOST") || "host",
-                        port: configService.get("DATABASE_PORT") || 5432,
-                    };
-                }
                 return {
                     type: "postgres",
-                    ...dbInfo,
+                    host: configService.get("DATABASE_HOST") || "host",
+                    port: configService.get("DATABASE_PORT") || 5432,
                     username: configService.get("DATABASE_USER") || "postgres",
                     password: configService.get("DATABASE_PASSWORD") || "admin",
                     database: configService.get("DATABASE_NAME") || "jii-comic",
-                    ssl: true,
+                    // ssl: true,
                     // Auto import entities
                     // -> No need to import outside `app.module`
                     autoLoadEntities: true,
